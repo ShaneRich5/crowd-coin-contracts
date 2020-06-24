7444c0a8a56095d742310794492d3b0a3aec71c1
Shane Richards <shane.richards@fticonsulting.com>
2020-06-23 22:55:34 -0400

    updated the campaign factory to use the correct types to manage campaign correctly

--- campaign.sol
+++ campaign.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.7.0;
 
+contract CampaignFactory {
+    Campaign[] public deployedCampaigns;
+
+    function createCampaign(uint256 minimum) public {
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (Campaign[] memory) {
+        return deployedCampaigns;
+    }
+}
+
 contract Campaign {
     struct Request {
         uint256 value;
